002f6d99389757f98666b7ab96dea1e77ac39bb5
bhuwanpandey7 <bp.pandey7@gmail.com>
2025-08-15 16:03:44 +0530

    Responsive menu for smaller view - TODO - refactor nav to seprate reusabe component [BP]

--- realtime-stocks/src/app/components/market-stocks/stocks/stocks.ts
+++ realtime-stocks/src/app/components/market-stocks/stocks/stocks.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockListComponent } from '../stock-list/stock-list';
 
@@ -7,10 +7,12 @@ import { StockListComponent } from '../stock-list/stock-list';
   standalone: true,
   imports: [CommonModule, StockListComponent],
   templateUrl: './stocks.html',
-  styleUrls: ['./stocks.scss']
+  styleUrls: ['./stocks.scss'],
 })
 export class StocksComponent {
   isLoading = false;
+  isNavCollapsed = true;
+  // create separate enum
   markets = [
     'US',
     'Europe',
@@ -19,11 +21,28 @@ export class StocksComponent {
     'Canada',
     'South America',
     'Africa',
-    'Middle East'
+    'Middle East',
   ];
   selectedMarket = 'US';
 
+  @HostListener('window:resize')
+  onResize() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (window.innerWidth >= 992) {
+      navbarCollapse?.classList.remove('show');
+    }
+  }
+
   selectMarket(market: string) {
     this.selectedMarket = market;
+    this.isNavCollapsed = true;
+    this.closeNavbar();
+  }
+
+  closeNavbar() {
+    const navEl = document.getElementById('marketNav');
+    if (navEl?.classList.contains('show')) {
+      navEl.classList.remove('show');
+    }
   }
 }
